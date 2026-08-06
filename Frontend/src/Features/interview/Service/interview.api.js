dd96d8d1637d1e3a970c5b3fcf5267925938041c
Priyanshu Sahu <priyanshusahu708@gmail.com>
2026-08-06 12:05:14 +0530

    Update interview.api.js

--- Frontend/src/Features/interview/Service/interview.api.js
+++ Frontend/src/Features/interview/Service/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://ai-job-preperation-plan.onrender.com",
     withCredentials: true,
 })
 
