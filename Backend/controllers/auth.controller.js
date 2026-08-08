2758239e782bbe284fe57fc8689afdc3e6299304
Priyanshu Sahu <priyanshusahu708@gmail.com>
2026-08-08 12:15:12 +0530

    Update auth.controller.js

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -75,7 +75,14 @@ async function loginController(req,res){
 
                 process.env.JWT_SCRETE
         )
-        res.cookie("token", token) 
+        res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
+        
         res.status(201).json({
             message:"User Login Successfully",
             user:{
