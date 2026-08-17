0abea4e897447709829a8b77a0fd9775917c167f
Priyanshu Sahu <priyanshusahu708@gmail.com>
2026-08-17 12:08:32 +0530

    Update auth.middleware.js

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 async function User(req, res, next) {
   try {
-    const token = req.cookies?.token;
+    const token = req.cookies.token;
 
     if (!token) {
       return res.status(401).json({
